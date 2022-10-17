40224c317e560c931905028f4315dde7683614a6
rafael.verona <raverona@gmail.com>
2022-10-16 21:46:19 -0300

    Add implementation without shortcuts

--- test/clojure_coding_challenges/strings/is_unique_test.clj
+++ test/clojure_coding_challenges/strings/is_unique_test.clj
@@ -1,7 +1,7 @@
 (ns clojure-coding-challenges.strings.is-unique-test
-  (:require [clojure.test :refer [deftest testing is use-fixtures]]
+  (:require [clojure.test :refer [are deftest testing is use-fixtures]]
             [clojure.test.check.clojure-test :refer [defspec]]
-            [clojure-coding-challenges.strings.is-unique :refer [is-unique?]]
+            [clojure-coding-challenges.strings.is-unique :refer [is-unique? is-unique?*]]
             [schema.test :refer [validate-schemas]]
             [clojure.test.check.properties :as prop]
             [clojure.test.check.generators :as gen]))
@@ -12,16 +12,26 @@
 
 (deftest is-unique-test
   (testing "Returns true for strings composed of unique characters only"
-    (is (is-unique? "asdqwertyfgh")))
+    (let [unique-string "asdqwertyfgh"]
+      (are [x] x
+               (is-unique? unique-string)
+               (is-unique?* unique-string))))
 
   (testing "Returns false for strings composed of non-unique characters"
-    (is (not (is-unique? "asdqwertyfgha"))))
+    (let [non-unique-string "asdqwertyfgha"]
+      (are [x] (not x)
+               (is-unique? non-unique-string)
+               (is-unique?* non-unique-string))))
 
   (testing "Returns true for empty string"
-    (is (is-unique? "")))
+    (are [x] x
+             (is-unique? "")
+             (is-unique?* "")))
 
-  (testing "Returns false for nil"
-    (is (is-unique? nil))))
+  (testing "Returns true for nil"
+    (are [x] x
+             (is-unique? nil)
+             (is-unique?* nil))))
 
 ; Generative tests
 
@@ -31,7 +41,8 @@
 
 (defspec is-unique 1000
   (prop/for-all [unique-string unique-string-generator]
-    (is-unique? unique-string)))
+    (is-unique? unique-string)
+    (is-unique?* unique-string)))
 
 (def non-unique-string-generator
   (gen/fmap (fn [xs] (apply str (concat xs xs)))
@@ -39,4 +50,5 @@
 
 (defspec is-not-unique 1000
   (prop/for-all [non-unique-string non-unique-string-generator]
-    (not (is-unique? non-unique-string))))
+    (not (is-unique? non-unique-string))
+    (not (is-unique?* non-unique-string))))
